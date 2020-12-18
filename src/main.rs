c6078a1a319068b3ee0928e42b42ca688b16b938
issei_itagaki <i-itagaki@atware.co.jp>
2020-12-18 18:10:57 +0900

    TODOの内容を表示できる

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ mod todo_type;
 
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
-use crate::model::DataBaseConnector;
+use crate::model::{DataBaseConnector, TodoData, Date};
+use crate::todo_type::TodoDataType;
+
+const DATABASE_PATH: &str = "./test_db.db3";
 
 #[get("/")]
 fn hello() -> &'static str{
@@ -16,7 +19,29 @@ fn hello() -> &'static str{
 
 #[get("/index")]
 fn index() -> Template{
-    let mut context:HashMap<String,String> = HashMap::new();
+    let connection_base = DataBaseConnector{ path: String::from(DATABASE_PATH) };
+
+    let mut context:HashMap<String,Vec<TodoData>> = HashMap::new();
+
+    let mut vec_of_do=vec![];
+    let mut vec_of_doing = vec![];
+    let mut vec_of_done = vec![];
+    match connection_base.get_vector_data_of_todo_type(TodoDataType::Do) {
+        Ok(vec)=>{vec_of_do = vec;}
+        Err(_)=>{}
+    }
+    match connection_base.get_vector_data_of_todo_type(TodoDataType::Doing) {
+        Ok(vec)=>{vec_of_doing = vec;}
+        Err(_)=>{}
+    }
+    match connection_base.get_vector_data_of_todo_type(TodoDataType::Done) {
+        Ok(vec)=>{vec_of_done = vec;}
+        Err(_)=>{}
+    }
+
+    context.insert("do".to_string(),vec_of_do);
+    context.insert("doing".to_string(),vec_of_doing);
+    context.insert("done".to_string(),vec_of_done);
     Template::render("index",context)
 }
 
@@ -27,8 +52,21 @@ fn register(todo: String) -> &'static str{
 }
 
 fn main(){
-    let connection_base = DataBaseConnector{ path: String::from("./test_db.db3") };
+    let connection_base = DataBaseConnector{ path: String::from(DATABASE_PATH) };
     connection_base.create_table();
+
+    //サンプル挿入
+    let sample_date = Date{year: 2020, month: 11, day: 5};
+    let sample_data_of_do = TodoData{id: 1, date: sample_date, detail: "study with friends".to_string()};
+    connection_base.insert_data_of_do(sample_data_of_do);
+
+    let sample_date2 = Date{year: 2020, month: 11, day: 5};
+    let sample_data_of_doing = TodoData{id: 2, date: sample_date2, detail: "go to travel".to_string()};
+    connection_base.insert_data_of_do(sample_data_of_doing);
+
+    let sample_date3 = Date{year: 2020, month: 11, day: 5};
+    let sample_data_of_done = TodoData{id: 3, date: sample_date3, detail: "sleep with friends".to_string()};
+    connection_base.insert_data_of_do(sample_data_of_done);
 
     rocket::ignite().mount("/",routes![register, hello, index]).attach(Template::fairing()).launch();
 }
--- src/model.rs
+++ src/model.rs
@@ -1,17 +1,17 @@
 use rusqlite::{params, Connection};
 use std::collections::HashMap;
 use crate::todo_type::TodoDataType;
-
-struct Date{
-    year :i64,
-    month :i64,
-    day :i64
+#[derive(serde::Serialize)]
+pub struct Date{
+    pub year :i64,
+    pub month :i64,
+    pub day :i64
 }
-
- pub struct TodoData{
-    id :i64,
-    date :Date,
-    detail :String
+#[derive(serde::Serialize)]
+pub struct TodoData{
+    pub id :i64,
+    pub date :Date,
+    pub detail :String
 }
 
 pub struct DataBaseConnector{
@@ -130,6 +130,54 @@ impl DataBaseConnector{
         Ok(index_map)
 
     }
+
+    pub fn get_vector_data_of_todo_type(&self, data_type:TodoDataType) -> Result<Vec<TodoData>,String>{
+        let connection = Connection::open(&self.path);
+        if connection.is_err(){
+            return Err("Cannot Connect DataBase".to_string());
+        }
+        let manage = connection.unwrap();
+
+        let flag;
+        match data_type{
+            TodoDataType::Do => {flag = "\"do\""},
+            TodoDataType::Doing => {flag = "\"doing\""},
+            TodoDataType::Done => {flag = "\"done\""}
+        }
+
+        let raw_select_query = format!("{}{}","select id,year,month,day,detail From list WHERE flag = ", flag);
+
+        let mut select_query = manage.prepare(raw_select_query.as_str());
+        if select_query.is_err(){
+            return Err("Cannot select query".to_string());
+        }
+        let mut select_query_result = select_query.unwrap();
+
+        let itr = select_query_result.query_map(params![],|row|{
+            Ok(TodoData{
+                id: row.get(0).unwrap(),
+                date: Date{year: row.get(1).unwrap(), month: row.get(2).unwrap(), day: row.get(3).unwrap()},
+                detail: row.get(4).unwrap()
+            })
+        });
+
+        if itr.is_err(){
+            return Err("Cannot select query2".to_string());
+        }
+        let itr_result = itr.unwrap();
+
+        let mut index_vec = vec![];
+        let mut count = 0;
+        for value in itr_result{
+            if value.is_err(){
+                return Err("Cannot select query3".to_string());
+            }
+            index_vec.insert(count,value.unwrap());
+            count = count +1;
+        }
+        Ok(index_vec)
+
+    }
 }
 
 
@@ -205,6 +253,30 @@ mod test{
 
         let result_map = connection_base.get_data_of_todo_type(TodoDataType::Do);
         match result_map{
+            Ok(con) => {
+                assert_eq!(con.len(),3)
+            }
+            Err(word) => {println!("{}",word)}
+        }
+    }
+    #[test]
+    fn test_get_vec_data_database(){
+        let connection_base = DataBaseConnector{ path: String::from("./test_db.db3") };
+        connection_base.create_table();
+        let sample_date = Date{year: 2020, month: 11, day: 5};
+        let sample_data_of_do = TodoData{id: 1, date: sample_date, detail: "study with friends".to_string()};
+        connection_base.insert_data_of_do(sample_data_of_do);
+
+        let sample_date2 = Date{year: 2020, month: 11, day: 5};
+        let sample_data_of_doing = TodoData{id: 2, date: sample_date2, detail: "go to travel".to_string()};
+        connection_base.insert_data_of_do(sample_data_of_doing);
+
+        let sample_date3 = Date{year: 2020, month: 11, day: 5};
+        let sample_data_of_done = TodoData{id: 3, date: sample_date3, detail: "sleep with friends".to_string()};
+        connection_base.insert_data_of_do(sample_data_of_done);
+
+        let result_vec = connection_base.get_vector_data_of_todo_type(TodoDataType::Do);
+        match result_vec{
             Ok(con) => {
                 assert_eq!(con.len(),3)
             }
